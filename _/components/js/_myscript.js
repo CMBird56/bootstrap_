1da08efeeaee2d81c34b4bfa447447cfc878128f
Philip <filkos56@gmail.com>
2015-05-15 19:39:39 +0200

    KONACNA VERZIJA

--- _/components/js/_myscript.js
+++ _/components/js/_myscript.js
@@ -1,10 +1,22 @@
 $(function() {
-    
-    // activae schedule tabs
-    
-    var hash = window.location.hash; //VAZNO! :ako URL ima na kraju #nesto , da ce nam 'nesto'
-    hash && $('ul.nav a[href="' + hash + '"]').tab('show'); // ' posle "" zato sto nam treba tekst
-// ovo hash && na pocetku znaci, da ako bude imalo hasha odradi ce sledece...
+
+	//activate schedule tabs
+	var hash = window.location.hash;
+	hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+	//controlling opening of accordions
+	$('#monday.accordion-body').addClass('in');
+
+	$('#artists #tuesday.accordion-body').addClass('in');
+	$('#artists #wednesday.accordion-body').addClass('in');
+	$('#artists #thursday.accordion-body').addClass('in');
+	$('#artists #friday.accordion-body').addClass('in');
+
+	//add bootstrap classes to photos
+	$('.abouttheartists img').addClass('img-circle');
+	$('aside.photosfromlastyear img').addClass('img-thumbnail');
+	$('.artistinfo .photogrid img').addClass('img-circle');
+
 
 	//highlight the current nav
 	$("#home a:contains('Home')").parent().addClass('active');
@@ -13,28 +25,30 @@ $(function() {
 	$("#venuetravel a:contains('Venue/Travel')").parent().addClass('active');
 	$("#register a:contains('Register')").parent().addClass('active');
 
+
 	//make menus drop automatically
 	$('ul.nav li.dropdown').hover(function() {
 		$('.dropdown-menu', this).fadeIn();
 	}, function() {
 		$('.dropdown-menu', this).fadeOut('fast');
 	});//hover
-    
-    //show tooltips   
-    $("[data-toggle='tooltip']").tooltip({ animation: true}); // podseti se JQ selektora
-    
-    // show modals
-    
-    $('.modalphotos img').on('click', function(){ // .modal je funkcija iz BOOTSTRAP-a 
-    $('#modal').modal({
-        show: true,
-    })
-    
-    var mysrc = this.src.substr(0,this.src.length-7) + '.jpg';
-    $('#modalimage').attr('src', mysrc);
-    $('#modalimage').on('click', function(){
-        $('#modal').modal('hide')
-        })   // hide modal 
-    }); // show modal
+
+	//show tooltips
+	$("[data-toggle='tooltip']").tooltip({ animation: true});
+
+	//show modals
+
+	$('.modalphotos img').on('click', function() {
+		$('#modal').modal({
+			show: true,
+		})
+
+		var mysrc = this.src.substr(0, this.src.length-7) + '.jpg';
+		$('#modalimage').attr('src', mysrc);
+		$('#modalimage').on('click', function(){
+				$('#modal').modal('hide');
+		})//hide modal
+	});//show modal
+
 
 }); //jQuery is loaded
